17858e96bd4154a3b284fd9af5d10696df41c4b0
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-02-14 22:52:41 +0300

    game menu
    show gallows and menu sign by clicking category sign.
    cowboy divided into pieces to add a piece with a wrong answer

--- HangMan.js
+++ HangMan.js
@@ -1,4 +1,8 @@
 const bush = document.getElementById('bush')
+const sign = document.getElementById('categories')
+const gallows = document.getElementById('gallows')
+const menuSign = document.getElementById('menu')
+
 let Question = function(question , answer){
     this.question = question;
     this.answer = answer;
@@ -11,11 +15,43 @@ var n = new Question('naber','iyi senden')
 console.log(n)
 console.log(n.selected)
 
-// var x = document.getElementById('artButton').onclick = function(){
-//     bush.style.left = '1700px'
-//     bush.style.transform = 'rotate(3600deg)'
+var x = document.getElementById('artButton').onclick = function(){
+    bush.style.left = '1700px'
+    bush.style.transform = 'rotate(3600deg)'
     
-// }
+}
 
-//Bush()
+
+
+// Button click functions
+function hideSignButtons(){
+    sign.style.right = '-400px'
+    setTimeout(function(){hide(sign);},1000)
+    show(gallows)
+    setTimeout(function(){gallows.style.left  = '0px';},500)
+    show(menuSign)
+    setTimeout(function(){menuSign.style.right  = '-70px';},500)
+}
+
+function hide(x){
+    x.classList.add('hide')
+}
+function show(x){
+    x.classList.remove('hide')
+}
+function historyButton(){
+    hideSignButtons()
+}
+function artButton(){
+    hideSignButtons()
+}
+function geographyButton(){
+    hideSignButtons()
+}
+function sportButton(){
+    hideSignButtons()
+}
+function scienceButton(){
+    hideSignButtons()
+}
 
