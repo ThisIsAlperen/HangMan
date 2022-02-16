cfa5f90f293f3fc7b1da7b3f50ea9dcfa5057223
ThisIsAlperen <aydin.m.alperen@gmail.com>
2022-02-16 19:41:06 +0300

    Summary
    The game is finished. a few things will be added.

--- HangMan.js
+++ HangMan.js
@@ -11,61 +11,63 @@ const head = document.getElementById('head')
 const body = document.getElementById('body')
 const arms = document.getElementById('arms')
 const wholeBody = document.getElementById('wholeBody')
+const result = document.getElementById('resultSub')
+const Win = document.getElementById('win')
+const Loose = document.getElementById('loose')
+const correct = document.getElementById('correct')
+
 let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 let wrongAnswer = 0;
-let answer2 = 'Loremipsum'
+
 
 var HistoryQuestions = [{
-    Question: 'Who is the king of the Greeks at the time of Trojan Horse',
+    Question: 'Who is the king of the Greeks at the time of Trojan Horse built?',
     Answer: 'Agamemnon'
 },
 {
-    Question: 'What is the name of the legendary sword of King Arthur ',
+    Question: 'What is the name of the legendary sword of King Arthur? ',
     Answer: 'Excalibur'
 }
 ]
-
-// 0000000000000000000000000000000000000000000000000000
-
-const xhr = new XMLHttpRequest();
-
-xhr.onreadystatechange = function () {
-    
-    if (xhr.status === 4 && xhr.status === 200) {
-        
-    }
+var ArtQuestions = [{
+    Question: 'Who is the famous painter cut his ear?',
+    Answer: 'Van Gogh'
+},
+{
+    Question: 'Which European sculptor created the statue “David”?',
+    Answer: 'Michelangelo'
 }
-xhr.open('GET', 'HistoryQuestions.txt', true);
-xhr.send()
-
-//textSplit()
-
-function textSplit() {
-    x = xhr.responseType
-    console.log(x)
-    x = String(x)
-    console.log(x)
-    x = x.split('Question:')
-    console.log(x)
+]
+var GeographyQuestions = [{
+    Question: 'The Golden Gate is the symbol of what city?',
+    Answer: 'San Francisco'
+},
+{
+    Question: 'On which sea does Romania have a coastline?',
+    Answer: 'Black Sea'
 }
-
-// 0000000000000000000000000000000000000000000000000000
-
-// let Question = function(question , answer){
-//     this.question = question;
-//     this.answer = answer;
-// }
-
-// Question.prototype.selected = function(question,answer){
-//     this.total  = question + answer
-// };
-// var n = new Question('naber','iyi senden')
-// console.log(n)
-// console.log(n.selected)
+]
+var SportQuestions = [{
+    Question: 'Dolphin style is related to what sport?',
+    Answer: 'Swimming'
+},
+{
+    Question: 'What do you score in American and Canadian football',
+    Answer: 'Touchdown'
+}
+]
+var ScienceQuestions = [{
+    Question: 'How are the holes of Swiss cheese made?',
+    Answer: 'Carbon dioxide'
+},
+{
+    Question: 'What science studies the weather? ',
+    Answer: ' Meteorology'
+}
+]
 
 setTimeout(Bush, 5000)
 function Bush() {
-    // show(bush)
     bush.style.left = '1700px'
     bush.style.transform = 'rotate(3600deg)'
     setInterval(Bush, 40000)
@@ -81,6 +83,7 @@ function bushReturn() {
 }
 // Button click functions
 function hideSignButtons() {
+
     sign.style.right = '-400px'
     setTimeout(function () { hide(sign); }, 1000)
     show(gallows)
@@ -90,13 +93,19 @@ function hideSignButtons() {
     show(game)
     setTimeout(function () { game.style.opacity = '1'; }, 1000)
     show(keyboard)
-    setTimeout(function () { keyboard.style.opacity = '1'; }, 1000)
-    // setTimeout(function(){show(wholeBody);},1000)
+    setTimeout(function () { keyboard.style.opacity = '1'; }, 1000) 
 
 }
-function Return() {
-    show(sign)
-    setTimeout(function () { sign.style.right = '35px'; }, 1000)
+function Return(x) {
+    if (x) {
+        show(sign)
+        setTimeout(function () { sign.style.right = '35px'; }, 1000)
+    }
+    else{
+        show(result)
+        setTimeout(function () { result.style.opacity = '1'; }, 1000)
+        
+    }
     gallows.style.left = '-550px';
     setTimeout(function () { hide(gallows); }, 1000)
     menuSign.style.right = '-400px';
@@ -105,14 +114,32 @@ function Return() {
     setTimeout(function () { hide(game); }, 1000)
     keyboard.style.opacity = '0';
     setTimeout(function () { hide(keyboard); }, 1000)
+    clear()
 }
 function showAnswer() {
-    console.log(answer.children[2].children)
+  
     for (i = 0; i < answer.children.length; i++) {
         answer.children[i].children[0].style.opacity = '1'
     }
-}
 
+}
+function clear() {
+
+    wrongAnswer = 0;
+    answer.innerHTML = ''
+    
+    for(i=0;i<keys.children.length;i++){
+        if(keys.children[i].classList[1] == 'disabled'){
+            keys.children[i].classList.remove('disabled')
+        }
+       
+    }
+    setTimeout(function () { hide(head); }, 1000)
+    setTimeout(function () { hide(body); }, 1000)
+    setTimeout(function () { hide(arms); }, 1000)
+    setTimeout(function () { hide(wholeBody); }, 1000)
+
+}
 function hide(x) {
     x.classList.add('hide')
 }
@@ -125,15 +152,19 @@ function historyButton() {
 }
 function artButton() {
     hideSignButtons()
+    Game('art')
 }
 function geographyButton() {
     hideSignButtons()
+    Game('geography')
 }
 function sportButton() {
     hideSignButtons()
+    Game('sport')
 }
 function scienceButton() {
     hideSignButtons()
+    Game('science')
 }
 
 //Create keyboard
@@ -151,7 +182,7 @@ function CreateKeys() {
 }
 
 window.onclick = function (e) {
-    console.log(e.target)
+  
 }
 
 function Answer(x) {
@@ -168,36 +199,55 @@ function Answer(x) {
         text.innerText = x[i]
         parent.appendChild(text)
         answer.appendChild(parent)
+        
+        if (text.innerText == '') {
+            parent.style.opacity = '0'
+            parent.classList.add('done')
+        }
     }
 }
 function Game(x) {
-    if (x == 'history') {
-        q = HistoryQuestions[0].Question
-        a = HistoryQuestions[0].Answer
-
+    r = Math.floor(Math.random()*2)
+  
+    if (x === 'history') {
+        q = HistoryQuestions[r].Question
+        a = HistoryQuestions[r].Answer
     }
-    if (x == 'art') {
-
+    if (x === 'art') {
+        q = ArtQuestions[r].Question
+        a = ArtQuestions[r].Answer
     }
-    if (x == 'geography') {
-
+    if (x === 'geography') {
+        q = GeographyQuestions[r].Question
+        a = GeographyQuestions[r].Answer
     }
-    if (x == 'sport') {
-
+    if (x === 'sport') {
+        q = SportQuestions[r].Question
+        a = SportQuestions[r].Answer
     }
-    if (x == 'science') {
-
+    if (x === 'science') {
+        q = ScienceQuestions[r].Question
+        a = ScienceQuestions[r].Answer
     }
+    
+    correct.innerText = 'Answer: '+ a
     question.innerText = q
     Answer(a)
 }
-document.getElementById('keys').onclick = function (e) {
+keys.onclick = function (e) {
     x = e.target.innerText
-    if (e.target.classList[1] != 'disabled') {
-        Play(x)
+    y = x.length
+    if(y<2){
+        
+        if (e.target.classList[1] != 'disabled') {
+            Play(x)
+            win()
+            
+        }
+        e.target.classList.add('disabled')
+    
     }
-    e.target.classList.add('disabled')
-
+   
 }
 function Play(x) {
     var respond = false
@@ -205,30 +255,66 @@ function Play(x) {
         if (x == answer.children[i].children[0].innerText) {
             answer.children[i].children[0].style.opacity = '1'
             respond = true
+            answer.children[i].classList.add('done')
         }
 
-        
+
     }
-    console.log(respond)
-    console.log(wrongAnswer)
-    if(respond === false){
-        wrongAnswer ++;
-        
+    if (respond === false) {
+        wrongAnswer++;
+
     }
-    if(wrongAnswer == 1){
+    if (wrongAnswer == 1) {
         show(head)
     }
-    if(wrongAnswer == 2){
+    if (wrongAnswer == 2) {
         show(body)
     }
-    if(wrongAnswer == 3){
+    if (wrongAnswer == 3) {
         show(arms)
     }
-    if(wrongAnswer == 4){
+    if (wrongAnswer == 4) {
         show(wholeBody)
     }
-    if(wrongAnswer == 5){
-        
+    if (wrongAnswer == 5) {
+        loose()
+        setTimeout(function () { Loose.style.opacity = '1'; }, 1500)
+    }
+
+}
+function win() {
+    var letters = []
+    
+    for (i = 0; i < answer.children.length; i++) {
+        if (answer.children[i].classList.length > 1) {
+            letters.push(answer.children[i])
+            
+        }
     }
     
+    if (letters.length == answer.children.length && letters.length>0) {
+        x = false
+        
+        setTimeout(function () { Return(x); }, 3000)
+        setTimeout(function () { Win.style.opacity = '1'; }, 4500)
+
+    }
+}
+
+function loose() {
+   
+    x = false
+    Return(x)
+    
+    setTimeout(function () { correct.style.opacity = '1'; }, 3500)
+
+}
+function OK(){
+    Win.style.opacity = '0';
+    Loose.style.opacity = '0';
+    result.style.opacity = '0';
+    setTimeout(function () { hide(result); }, 1000)
+    Return(x=true)
+    setTimeout(function () { correct.style.opacity = '0' ; }, 1000)
+
 }
